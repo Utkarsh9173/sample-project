5502e2d3c24135f53cf770ac1b3b1ccf91230838
Utkarsh Shukla <utkarsh@studiographene.com>
2021-12-20 12:14:39 +0530

    signup

--- src/database/repository/user.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "@database/model/user.model";
-
-@EntityRepository(User)
-export class UserRepo extends Repository<User> {}
