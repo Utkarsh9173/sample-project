147c304acb5b4e39d82a7ba2b3e219b344cf000c
Utkarsh Shukla <utkarsh@studiographene.com>
2021-12-20 12:13:32 +0530

    signup

--- src/api/route/base.route.ts
+++ src/api/route/base.route.ts
@@ -29,59 +29,6 @@ class BaseRoute {
 
   private assign() {
     this.router.post(
-      "/register",
-      this.httpRequestValidator.validate("body", register),
-      this.baseController.register
-    );
-
-    this.router.get(
-      "/user-email-verification/:uniqueKey",
-      // this.httpRequestValidator.validate("query", verifyOtp),
-      this.baseController.verifyUserEmail
-    );
-
-    this.router.post(
-      "/login",
-      this.authenticate,
-      this.httpRequestValidator.validate("body", login),
-      this.baseController.login
-    );
-
-    // this.router.post(
-    //   "/reset-password",
-    //   this.httpRequestValidator.validate("body", resetPassword),
-    //   this.baseController.resetPasswordInit
-    // );
-
-    // this.router.get(
-    //   "/otp-verifier",
-    //   this.httpRequestValidator.validate("query", verifyOtp),
-    //   this.baseController.verifyResetPasswordOtp
-    // );
-
-    // this.router.post(
-    //   "/update-password",
-    //   this.httpRequestValidator.validate("body", updatePassword),
-    //   this.baseController.updatePassword
-    // );
-
-    // this.router.post(
-    //   "/account-setup",
-    //   this.authenticate,
-    //   this.httpRequestValidator.validate("body", accountSetup),
-    //   this.baseController.completeUserAccount
-    // );
-
-    // this.router.post(
-    //   "/social-login",
-    //   this.httpRequestValidator.validate("body", socialLogin),
-    //   this.baseController.socialLogin
-    // );
-
-    // this.router.get("/country-codes", this.baseController.getCountryCodes);
-    this.router.get("/", this.baseController.defaultCheck);
-
-    this.router.post(
       "/signup",
       this.httpRequestValidator.validate("body", signupValidator),
       this.baseController.Signup
